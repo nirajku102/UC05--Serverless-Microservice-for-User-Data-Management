e0e35a73231430108a311424c0bb7e3bc69c274e
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-03-17 09:04:21 +0000

    adding routes

--- lambda/create_user/app.py
+++ lambda/create_user/app.py
@@ -7,7 +7,7 @@ dynamodb = boto3.resource('dynamodb')
 table_name = os.environ['USERS_TABLE']
 table = dynamodb.Table(table_name)
 
-def lamda_handler(event, context):
+def lambda_handler(event, context):
     try:
         user_data = json.loads(event['body'])
     except:
@@ -16,24 +16,23 @@ def lamda_handler(event, context):
             'body': json.dumps({'error': 'Invalid request body'})
         }
 
-    
-    user_id = sts(uuid.uuid4())
+    user_id = str(uuid.uuid4())
 
     item = {
-        'user_id': user_id
-        'name': user_data['name']
+        'user_id': user_id,
+        'name': user_data['name'],
         'email': user_data['email']
-
     }
+    
     try:
-        table.put_item(item=item)
+        table.put_item(Item=item)
         return {
-                'statusCode': 200,
-                'body': json.dumps({'user_id': user_id})
-    }
-
+            'statusCode': 200,
+            'body': json.dumps({'user_id': user_id})
+        }
     except Exception as e:
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
         }
+        
