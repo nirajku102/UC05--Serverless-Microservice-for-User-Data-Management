f571133803c7f1c50259c62dd7dbc20ab85bcd02
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-03-17 07:43:08 +0000

    adding userfile

---
+++ lambda/create_user/app.py
@@ -0,0 +1,39 @@
+import json
+import os
+import uuid
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ['USERS_TABLE']
+table = dynamodb.Table(table_name)
+
+def lamda_handler(event, context):
+    try:
+        user_data = json.loads(event['body'])
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Invalid request body'})
+        }
+
+    
+    user_id = sts(uuid.uuid4())
+
+    item = {
+        'user_id': user_id
+        'name': user_data['name']
+        'email': user_data['email']
+
+    }
+    try:
+        table.put_item(item=item)
+        return {
+                'statusCode': 200,
+                'body': json.dumps({'user_id': user_id})
+    }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
